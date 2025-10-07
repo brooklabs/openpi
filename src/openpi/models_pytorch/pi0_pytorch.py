9612b7eca9f241e87621054f530ef86f9e8fe672
dhpitt <dave@ultra.tech>
2025-10-07 18:24:13 +0000

    draft: sample actions w/prefix

--- src/openpi/models_pytorch/pi0_pytorch.py
+++ src/openpi/models_pytorch/pi0_pytorch.py
@@ -459,3 +459,81 @@ class PI0Pytorch(nn.Module):
         suffix_out = suffix_out[:, -self.config.action_horizon :]
         suffix_out = suffix_out.to(dtype=torch.float32)
         return self.action_out_proj(suffix_out)
+
+    def clipped_guidance_weight(self, t: Tensor, beta: float = 5.0) -> Tensor:
+        """
+        Given time t, compute the clipped guidance weight according to eq.s (2) and (4)
+        in the RTC paper, https://arxiv.org/pdf/2506.07339
+        """
+        r_tau_sq = ((1 - t) ** 2) / (t ** 2 + (1 - t) ** 2)
+        return min(beta, (1 - t) / (t * r_tau_sq))
+
+    def compute_soft_mask(self, device, d: int, s: int):
+        """
+        Compute the mask w for the prefix s.t. guidance weight is 1 for frozen actions 0:d
+        and slowly ramps to 0 for d:H-s, then 0 for H-s:H
+
+        See eq. (5) in the RTC paper, https://arxiv.org/pdf/2506.07339
+        """
+        soft_mask = (self.config.action_horizon - s - torch.arange(self.config.action_horizon, device=device)) / (self.config.action_horizon - s - d + 1)
+        soft_mask = soft_mask * (torch.exp(soft_mask) - 1) / (math.e + 1)
+        soft_mask[:d] = 1.0
+        soft_mask[-s:] = 0.0
+        return soft_mask
+
+
+    @torch.no_grad()
+    def sample_actions_with_prefix(self, device, observation, prefix, d: int, s: int, beta: float = 5.0, noise=None, num_steps=10) -> Tensor:
+        """Do a full inference forward and compute the action (batch_size x num_steps x num_motors)"""
+        bsize = observation.state.shape[0]
+        if noise is None:
+            actions_shape = (bsize, self.config.action_horizon, self.config.action_dim)
+            noise = self.sample_noise(actions_shape, device)
+
+        # Right-pad prefix to the action horizon
+        if prefix.shape[1] < self.config.action_horizon:
+            prefix = torch.cat([prefix, torch.ones((bsize, self.config.action_horizon - prefix.shape[1], self.config.action_dim), device=device)], dim=1)
+   
+        W = self.compute_soft_mask(device, d, s)
+
+        images, img_masks, lang_tokens, lang_masks, state = self._preprocess_observation(observation, train=False)
+
+        prefix_embs, prefix_pad_masks, prefix_att_masks = self.embed_prefix(images, img_masks, lang_tokens, lang_masks)
+        prefix_att_2d_masks = make_att_2d_masks(prefix_pad_masks, prefix_att_masks)
+        prefix_position_ids = torch.cumsum(prefix_pad_masks, dim=1) - 1
+
+        # Compute image and language key value cache
+        prefix_att_2d_masks_4d = self._prepare_attention_masks_4d(prefix_att_2d_masks)
+        self.paligemma_with_expert.paligemma.language_model.config._attn_implementation = "eager"  # noqa: SLF001
+
+        _, past_key_values = self.paligemma_with_expert.forward(
+            attention_mask=prefix_att_2d_masks_4d,
+            position_ids=prefix_position_ids,
+            past_key_values=None,
+            inputs_embeds=[prefix_embs, None],
+            use_cache=True,
+        )
+
+        dt = -1.0 / num_steps
+        dt = torch.tensor(dt, dtype=torch.float32, device=device)
+
+        x_t = noise
+        time = torch.tensor(1.0, dtype=torch.float32, device=device)
+        while time >= -dt / 2:
+            expanded_time = time.expand(bsize)
+            v_t = self.denoise_step(
+                state,
+                prefix_pad_masks,
+                past_key_values,
+                x_t,
+                expanded_time,
+            )
+
+            error = (prefix - v_t) * W[None, :, None] # apply soft mask along time dimension
+            g = error * torch.autograd.grad(v_t.sum(), state, create_graph=True)[0] # compute vector-jacobian product (eq. 10)
+
+            # Euler step - use new tensor assignment instead of in-place operation
+            x_t = x_t + dt * (v_t + self.clipped_guidance_weight(expanded_time, beta) * g)
+            time += dt
+        return x_t
+
